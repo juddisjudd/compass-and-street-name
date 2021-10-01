b4c45f0f71135a512801a673083ca5a3fa730ab6
Night Owls Roleplay <85649796+nightowlsrp@users.noreply.github.com>
2021-09-30 22:38:50 -0700

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description "MsQuerade's Compass and Streetname HUD"
+version "1.0"
+
+client_scripts {
+	'client/essentials.lua',
+	'client/compass.lua',
+	'client/streetname.lua'
+}
